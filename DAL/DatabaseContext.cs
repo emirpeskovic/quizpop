e1390b34c0b213066d82fad3775c06eae25be600
Emir Peskovic <me@emir.dk>
2023-02-27 10:30:42 +0100

    reflection, snake case naming convention, entity registration, string extensions, dockerfile

--- DAL/DatabaseContext.cs
+++ DAL/DatabaseContext.cs
@@ -1,8 +1,58 @@
-﻿using System.Data.Entity;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Humanizer;
+using QuizPop.Extensions;
 
 namespace QuizPop.DAL
 {
     public class DatabaseContext : DbContext
     {
+        public DatabaseContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var entityMethod = typeof(ModelBuilder).GetMethod("Entity");
+            if (entityMethod == null) return;
+
+            foreach (var entityType in GetEntityTypes())
+            {
+                _ = entityMethod.MakeGenericMethod(entityType).Invoke(modelBuilder, null);
+            }
+
+            foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                entityType.SetTableName(entityType.DisplayName().ToSnakeCase());
+
+                foreach (IMutableProperty property in entityType.GetProperties())
+                {
+                    property.SetColumnName(property.Name.ToSnakeCase());
+                }
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("YourConnectionStringHere",
+                npgsqlOptionsAction: options =>
+                {
+                    options.SetPostgresVersion(new Version(13, 4));
+                });
+        }
+
+        public DbSet<T> Entity<T>() where T : class, IEntity
+        {
+            return base.Set<T>();
+        }
+
+        private static IEnumerable<Type> GetEntityTypes()
+        {
+            return Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => typeof(IEntity).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract);
+        }
     }
 }
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace QuizPop.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string str) => Regex.Replace(str, "([a-z])([A-Z])", "$1_$2").ToLower();
+    }
+}
